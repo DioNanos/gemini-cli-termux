bf40da48686b6f34e8a7051fba7a10354e52c1ad
Tommaso Sciortino <sciortino@gmail.com>
2026-05-14 19:01:38 -0700

    security: update dependencies to fix critical and high vulnerabilities (#27077)

--- packages/core/src/telemetry/gcp-exporters.ts
+++ packages/core/src/telemetry/gcp-exporters.ts
@@ -29,6 +29,10 @@ export class GcpTraceExporter extends TraceExporter {
       resourceFilter: /^gcp\./,
     });
   }
+
+  async forceFlush(): Promise<void> {
+    return Promise.resolve();
+  }
 }
 
 /**
